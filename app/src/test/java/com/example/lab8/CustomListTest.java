838db4a9d8df708d11354c846e36d938e7525a92
claremaodou <claremaodou3@gmail.com>
2022-10-31 16:53:25 -0600

    hasCityTest should fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -32,49 +32,52 @@ public class CustomListTest {
 
 
     @Test
-          public void addCityTest(){
-              list = MockCityList();
-              int listSize = list.getCount();list.addCity(new City("Estevan", "SK"));
-             assertEquals(list.getCount(),listSize + 1);
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
          }
+
+
+
+
+     @Test
+     public void hasCityTest(){
+      list = MockCityList();
+      list.addCity(new City("Calgary", "AB"));
+      assertThat(list, contains("Calgary", "AB"));
+    }
+
+
+    /**
+     *      @Test
+     *      public void deleteCityTest(){
+     *          list = MockCityList();
+     *          int listSize = list.getCount();
+     *          list.addCity(new City("Regina", "SK"));
+     *          assertEquals(list.getCount(),1);
+     *          list.deleteCity(new City("Regina", "SK"));
+     *          assertEquals(list.getCount(),0);
+     *      }
+     */
+
 
 
 
     /**
      *     @Test
-     *     public void hasCityTest(){
+     *     public void countCityTest(){
      *         list = MockCityList();
-     *         list.addCity(new City("Calgary", "AB"));
-     *         city = get
-     *         assertThat(list, contains("Calgary", "AB"));}
+     *         list.addCity(new City("Red Deer", "AB"));
+     *         list.addCity(new City("Lethbridge", "AB"));
+     *         list.addCity(new City("Edmonton", "AB"));
+     *         int listSize_new = 3;
+     *         assertEquals(list.getCount(),listSize_new);
+     *
+     *     }
      */
 
-
-
-     @Test
-     public void deleteCityTest(){
-         list = MockCityList();
-         int listSize = list.getCount();
-         list.addCity(new City("Regina", "SK"));
-         assertEquals(list.getCount(),1);
-         list.deleteCity(new City("Regina", "SK"));
-         assertEquals(list.getCount(),0);
-     }
-
-
-
-
-
-    @Test
-    public void countCityTest(){
-        list = MockCityList();
-        list.addCity(new City("Red Deer", "AB"));
-        list.addCity(new City("Lethbridge", "AB"));
-        list.addCity(new City("Edmonton", "AB"));
-        int listSize_new = 3;
-        assertEquals(list.getCount(),listSize_new);
-
-    }
 
 
 }
